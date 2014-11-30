980b1a7c8542d688d40ff6575f302aa3071cc944
mwettstein <mwettstein@users.noreply.github.com>
2014-11-30 13:53:50 +0100

    easy_fft.m hilfsfunktion als test

---
+++ utilities/easy_fft.m
@@ -0,0 +1,30 @@
+function [ out_data ] = easy_fft( in_data, fs )
+%EASY_FFT - quick & easy fft plot for dummies
+%   Detailed explanation goes here
+
+nrOfPoints=2^nextpow2(length(in_data));
+out_data=fft(in_data,nrOfPoints)/length(in_data);
+f=fs/2*linspace(0,1,nrOfPoints/2+1);
+%plot
+figure('units','normalized','outerposition',[0 0 1 1]);
+subplot(2,1,1)
+plot(in_data)
+title('time domain','FontSize',12)
+xlabel('samples(n)')
+ylabel('y(t)')
+subplot(2,1,2)
+plot(f,2*abs(out_data(1:nrOfPoints/2+1)))
+%mark maximum
+[my,ix]=find(abs(out_data(1:nrOfPoints/2+1))==max(abs(out_data(1:nrOfPoints/2+1))));
+line(f(ix),2*abs(out_data(ix)),...
+          'marker','s',...
+          'markerfacecolor',[0,0,0],...
+          'linestyle','none');   
+text(f(ix)+1/40*f(length(f)),2*abs(out_data(ix)),[num2cell(f(ix)) ' Hz'],...
+     'horizontalalignment','center',...
+          'fontsize',12); 
+        title('frequency domain','FontSize',12)
+xlabel('frequency(Hz)')
+ylabel('|Y(f)|')
+end
+
---
+++ utilities/easy_fft_test.m
@@ -0,0 +1,9 @@
+%% easy_fft test
+fs=48e3;
+nPoints= 1e4;
+t=0:1/fs:nPoints*1/fs;
+f1=3e3;
+f2=5e3;
+y=cos(2*pi*f1*t)+sin(2*pi*f2*t);
+
+easy_fft(y,fs);
